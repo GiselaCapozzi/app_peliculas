a8185fda5a923b1806fc527d1b7320c719b6c4bc
GiselaCapozzi <gisecapozzi@gmail.com>
2023-10-03 21:20:23 -0300

    -

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,7 @@ import { imageUrl } from "../constants/apiFilms"
 import sin_imagen from '../assets/sin_imagen.png';
 import styles from '../styles/card.module.css';
 import { useNavigate } from "react-router-dom";
+import { Loading } from "./Loading";
 
 export const Card = ({ imagen, titulo, classNameImg, id }) => {
 
