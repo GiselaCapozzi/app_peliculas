498c2b175090566e6aea013c2f259cb753df45b9
GiselaCapozzi <gisecapozzi@gmail.com>
2023-10-05 21:42:45 -0300

    Busqueda de peliculas y series con scroll infinito

--- src/services/searchAllPages.js
+++ src/services/searchAllPages.js
@@ -2,7 +2,6 @@ import { API_KEY } from "../constants/EnvVariables";
 
 export const searchAllPages = async (search, page, streaming) => {
   const result = await fetch(`https://api.themoviedb.org/3/search/${streaming}?&api_key=${API_KEY || 'a1a65561608eec5f512496b236209147'}&query=${search}&page=${page}`)
-  console.log(result)
   const data = await result.json();
   return data;
 }
